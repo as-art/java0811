311367fb326fa379528b91e6061d8fcb5f336462
MB-study <MB-study@192.168.1.238>
2020-08-19 14:28:51 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/case3/EDog.java
@@ -0,0 +1,17 @@
+package com.pcschool.ocp.d08.case3;
+
+
+
+
+public class EDog extends Dog {
+ 
+	public void eat() {
+            System.out.println("電");
+	}
+	 
+	public void skill() {
+            System.out.println("玩");
+	}
+	 
+}
+ 
