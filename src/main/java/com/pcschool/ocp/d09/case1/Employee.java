d23ad2515cd89ceb5fc33192f9595b4081d8b9d8
MB-study <MB-study@192.168.1.238>
2020-08-20 09:44:48 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d09/case1/Employee.java
@@ -0,0 +1,29 @@
+
+package com.pcschool.ocp.d09.case1;
+public class Employee {
+       String name = "月例會";  
+    class Job {
+      String name = "Java";
+        public void program(){
+          String name = ("寫程式");
+         System.out.println(name + ":" + this.name);
+            }
+        public  void meeting(){
+            System.out.println("開會");
+             System.out.println(name + ":" + Employee.this.name);
+        }
+        
+    }
+   
+    class Study{
+        public  void read(){
+            System.out.println("進修");
+            
+        }
+        
+    }
+
+
+
+
+}
