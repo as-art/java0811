77149cf60a8016947d797481dcacb81a25c03f54
MB-study <MB-study@192.168.1.238>
2020-08-21 16:03:48 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d10/case6/VeatorDemo.java
@@ -0,0 +1,17 @@
+
+package com.pcschool.ocp.d10.case6;
+
+import java.util.Random;
+import java.util.Vector;
+
+
+public class VeatorDemo {
+    public static void main(String[] args) throws Exception{
+        Vector<Integer> v = new  Vector<>(25, 5);
+        for (int i=1;i<=25;i++) {
+           v.add(new Random().nextInt(100));
+            System.out.printf("capacity: %d size: %02d %s\n", v.capacity(), v.size(), v);
+            Thread.sleep(1000);
+        }
+    }
+}
